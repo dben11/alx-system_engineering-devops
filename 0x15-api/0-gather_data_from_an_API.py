01e942df287fc2c3dff50e1eba6bfa97853f4112
dben <sam.dben@gmail.com>
2024-01-03 05:51:33 -0800

    gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" A python script that uses a REST API for a given employee
+you must use urllib or request module
+the script must accept an integer as a parameter.
+"""
+
+import requests
+from sys import argv
+
+# Code should not be executed when imported
+if __name__ == "__main__":
+    try:
+        empId = int(argv[1])
+    except ValueError:
+        exit()
+
+# Creating a rest api
+api_url = 'https://jsonplaceholder.typicode.com'
+user_uri = '{}/users/{}'.format(api_url, empId)
+todo_uri = '{}/todos'.format(user_uri)
+
+# User response
+response = requests.get(user_uri).json()
+
+# Name of employee
+nameOf_emp = response.get('name')
+
+# user todo response
+todoResp = requests.get(todo_uri).json()
+
+# Total number of task
+TotalNum_task = len(todoResp)
+
+# Total number of uncompleted task
+uncompleted_task = sum([elem['completed'] is False for elem in todoResp])
+
+# Number of completed task
+completed_task = TotalNum_task - uncompleted_task
+
+# Format the expected output
+output = "Employee {} is done with tasks({}/{}):"
+print(output.format(nameOf_emp, completed_task, TotalNum_task))
+
+# Printing the completed tasks
+for elem in todoResp:
+    if elem.get("completed") is True:
+        print("\t", elem.get("title"))
---
+++ 0x15-api/README.md
@@ -0,0 +1,3 @@
+This is an API project.
+this is actually the first api project.
+this api project helps to understand how the front end and the back-end commumunicates.
